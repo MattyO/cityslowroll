a1e416b25ba07a026daaf72c69be8ded511c1215
Matt ODonnell <matt.odonnell@leandog.com>
2014-05-26 17:23:28 -0400

    im dumb

--- cityslowroll/cityslowroll/settings.py
+++ cityslowroll/cityslowroll/settings.py
@@ -65,8 +65,8 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, os.environ['CITYSLOWROLL_DB_NAME']),
         'USER': os.path.join(BASE_DIR, os.environ['CITYSLOWROLL_DB_USER']) , 
         'PASSWORD': os.path.join(BASE_DIR, os.environ['CITYSLOWROLL_DB_PASSWORD']) ,
-        'HOST': os.path.join(BASE_DIR, os.environ['CITYSLOWROLL_DB_PORT']) , 
-        'PORT': os.path.join(BASE_DIR, os.environ['CITYSLOWROLL_DEBUG']) ,
+        'HOST': os.path.join(BASE_DIR, os.environ['CITYSLOWROLL_DB_HOST']) , 
+        'PORT': os.path.join(BASE_DIR, os.environ['CITYSLOWROLL_DB_PORT']) ,
     }
 }
 
